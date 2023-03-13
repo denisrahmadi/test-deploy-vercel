f90cacb1b03df6006adc764c79097a122fbf9654
unknown <you@example.com>
2023-03-13 21:19:36 +0700

    first commit test deploy to vercel

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1 class="teks">
+    BISAAAAA DONGGGGGGGG
+  </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: black;
+  }
+
+  .teks{
+    background-color: white;
+    padding: 10px;
+  }
+</style>
